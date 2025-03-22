509ad6f13bd2c606f916da4ec7d1e523167d7d24
talarys <d.simon.ssx0@gmail.com>
2025-03-22 16:41:01 +0100

    Added some configs

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_EXTRA_COLEMAKDH
+// #define MIRYOKU_CLIPBOARD_WIN
+
